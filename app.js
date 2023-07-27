1c505a7cc03aabf24fa7e842d6339061b05d6a38
dina2803 <dina@MacBook-Air-Dina.local>
2023-07-27 17:29:07 +0600

    dz3

---
+++ app.js
@@ -0,0 +1,9 @@
+function  summNamber(a){
+    console.log(typeof a)
+}
+let revString = (string) => {
+    let reverse = string.split('').reverse().join('')
+    console.log(reverse)
+}
+revString('hello')
+summNamber(true)
